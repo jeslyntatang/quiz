29841d3cb4f399207dc591c821eeab663ca77590
Jeslyn Tatang <jeslyn.tatang@gmail.com>
2018-12-05 02:06:16 +0000

    added css

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
-        var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
@@ -15,34 +14,35 @@ $(document).ready(function() {
         var num4 = question4(q4Result);
         var totalScore = num1 + num2 + num3 + num4;
         if (totalScore <= 4) {
-            $("#house").text("you're in wampus");
+            $("#house").text("Congrats, " + name + "! You're in Wampus");
+            $("#houseimg").attr("src","https://vignette.wikia.nocookie.net/harrypotter/images/e/e6/Wampus_ClearBG_2.png/revision/latest?cb=20161204224541");
         }
         else if (totalScore > 4 && totalScore <= 9) {
-            $("#house").text("you're in thunderbird");
+            $("#house").text("Congrats, " + name + "! You're in Thunderbird");
+            $("#houseimg").attr("src","https://vignette.wikia.nocookie.net/harrypotter/images/1/1a/Thundebird_ClearBG_2.png/revision/latest/scale-to-width-down/350?cb=20161204221417");
         }
         else if (totalScore > 9 && totalScore <= 10) {
-            $("#house").text("you're in pukwudgie");
+            $("#house").text("Congrats, " + name + "! You're in Pukwudgie");
+            $("#houseimg").attr("src","https://i.pinimg.com/originals/f0/76/fb/f076fb0c9dc4e4f269061e6cbd10e2f5.png");
         }
         else if (totalScore > 10) {
-            $("#house").text("you're in horned serpent");
+            $("#house").text("Congrats, " + name + "! You're in Horned Serpent");
+            $("#houseimg").attr("src","https://vignette.wikia.nocookie.net/harrypotter/images/3/3f/Horned_Serpent_ClearBG_2.png/revision/latest/scale-to-width-down/350?cb=20161204071312");
         }
+        console.log("this is " + totalScore);
     });
     
     function question1(q1Result) {
         var num1;
-        //wampus
         if (q1Result == "freedom") {
             num1 = 1;
         }
-        //thunderbird
         else if (q1Result == "individuality") {
             num1 = 2;
         }
-        //pukwudgie
         else if (q1Result == "service") {
             num1 = 3;
         }
-        //horned serpent
         else if (q1Result == "ambition") {
             num1 = 4;
         }
@@ -54,20 +54,16 @@ $(document).ready(function() {
     
     function question2(q2Result) {
         var num2;
-        //wampus
-        if (q2Result == "telekinesis") {
+        if (q2Result == "fantasy") {
             num2 = 1;
         }
-        //thunderbird
-        else if (q2Result == "enhanced speed") {
+        else if (q2Result == "adventure") {
             num2 = 2;
         }
-        //pukwudgie
-        else if (q2Result == "telepathy") {
+        else if (q2Result == "mystery") {
             num2 = 3;
         }
-        //horned serpent
-        else if (q2Result == "enhanced intelligence") {
+        else if (q2Result == "science fiction") {
             num2 = 4;
         }
         else {
@@ -78,20 +74,16 @@ $(document).ready(function() {
     
     function question3(q3Result) {
         var num3;
-        //wampus
-        if (q3Result == "pain") {
+        if (q3Result == "green") {
             num3 = 1;
         }
-        //thunderbird
-        else if (q3Result == "fear") {
+        else if (q3Result == "red") {
             num3 = 2;
         }
-        //pukwudgie
-        else if (q3Result == "regret") {
+        else if (q3Result == "yellow") {
             num3 = 3;
         }
-        //horned serpent
-        else if (q3Result == "shame") {
+        else if (q3Result == "blue") {
             num3 = 4;
         }
         else {
@@ -102,24 +94,20 @@ $(document).ready(function() {
     
     function question4(q4Result) {
         var num4; 
-        //wampus
         if (q4Result == "creature") {
             num4 = 1;
         }
-        //thunderbird
         else if (q4Result == "city") {
             num4 = 2;
         }
-        //pukwudgie
         else if (q4Result == "plant") {
             num4 = 3;
         }
-        //horned serpent
         else if (q4Result == "spell") {
             num4 = 3;
         }
         else {
-            window.alert("your answer t question 4 is not a valid input");
+            window.alert("your answer to question 4 is not a valid input");
         }
         return num4;
     }
